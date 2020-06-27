0f133e1e2563bb396273afc7b60fd09feec73d61
Aren <reactDOM.render@yandex.ru>
2020-06-27 17:27:28 +0300

    Updates

---
+++ precache-manifest.d7a2701d44f5bffafadc9492b50a7250.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69714045c86d16e840a5b420a352cdf7",
+    "url": "/socnetwork/index.html"
+  },
+  {
+    "revision": "a32b200e1d57be4bd522",
+    "url": "/socnetwork/static/css/main.503b3f31.chunk.css"
+  },
+  {
+    "revision": "177b0bcc3869fb117df1",
+    "url": "/socnetwork/static/js/2.a3ed275f.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/socnetwork/static/js/2.a3ed275f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32b200e1d57be4bd522",
+    "url": "/socnetwork/static/js/main.c337c4db.chunk.js"
+  },
+  {
+    "revision": "2e76584438004ab1bd8b",
+    "url": "/socnetwork/static/js/runtime-main.7bd589f7.js"
+  },
+  {
+    "revision": "37ccf83b63f29154abfde9ea748f1dbf",
+    "url": "/socnetwork/static/media/bgmenu.37ccf83b.jpeg"
+  },
+  {
+    "revision": "bff3404ef1525bb5504786b55b92a61f",
+    "url": "/socnetwork/static/media/defaultUser.bff3404e.jpg"
+  }
+]);
